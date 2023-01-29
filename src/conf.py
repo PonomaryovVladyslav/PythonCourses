bb87a19c510a62dbd471c6000db2ef39f0a30d3a
Serhii Horodilov <sgorodil@gmail.com>
2023-01-29 05:38:11 +0200

    Introduction to Python (#28)
    Added introduction part including Python brief history and pros, installation and usage guides.
    Added some static files to the repo and updated bib db with references.
    Added draft documents for future appendixes (included to toctree in the master document).

--- src/conf.py
+++ src/conf.py
@@ -12,13 +12,15 @@ sys.path.insert(0, str(BASE_DIR / "problem-sets" / "src"))
 project = "Python Training Course"
 project_copyright = \
     f"{datetime.now().year}, Python training course authors and contributors"
-authors = "Vladyslav Ponomaryov \\and Serhii Horodilov"
+authors = "Vladyslav Ponomaryov \\and " \
+          "Serhii Horodilov"
 version = "2022.11.dev"
 
 # general configuration
 master_doc = root_doc = "index"
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx.ext.imgconverter",
 
     "sphinxcontrib.bibtex",
     "sphinx_rtd_theme",
@@ -50,4 +52,8 @@ latex_doc = project.title().replace(" ", "") + ".tex"
 latex_engine = "xelatex"
 latex_documents = [
     (master_doc, latex_doc, project, authors, "manual", False),
+]
+latex_appendices = [
+    "appx/code_edit",
+    "appx/env_path",
 ]
