4e2227cb16ac3f3cc63bc2d75558692a72fb03ca
Serhii Horodilov <sgorodil@gmail.com>
2024-02-26 15:06:36 +0200

    GH-202: Add test pass/fail marks and update transitions

--- src/conf.js
+++ src/conf.js
@@ -3,6 +3,9 @@ import "../assets/favicon.ico"
 import "./_static/scss/presentation-common.scss"
 import "./_static/scss/presentation-normalization.scss"
 
+import "../assets/icons/check-mark-svgrepo-com.svg"
+import "../assets/icons/cross-mark-svgrepo-com.svg"
+
 // import impress.js presentation framework
 import "../assets/impress.js/js/impress"
 import "../assets/impress.js/css/impress-common.css"
